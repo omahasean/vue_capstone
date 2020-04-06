f82d253d78febf14138ca23f807465152e0230b9
Sean Griswold <omahasean@gmail.com>
2020-04-06 17:02:06 -0400

    fixing issue

--- vue/backend/database/create_db.sh
+++ vue/backend/database/create_db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
-<<<<<<< HEAD
+
 DATABASE=city-tour
-=======
+
 DATABASE=citytour
->>>>>>> 591ab6c101d40b28033efadfff438e3da9298a84
+
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
