d03dc72b82d729d96dd08b6d0f4f43c6c8d40810
Joseph Ramirez <joeyramirez.81@gmail.com>
2020-04-06 16:47:13 -0400

    contacted api

--- vue/frontend/src/views/Home.vue
+++ vue/frontend/src/views/Home.vue
@@ -1,12 +1,41 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <p>You made it</p>
+    <button @click="displayMap">Submit</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  components: {},
+  data() {
+    return {
+      landmark: {
+        name: '',
+        zipcode: ''
+      }
+    }
+  },
+  methods: {
+    displayMap() {
+      fetch(`http://dev.virtualearth.net/REST/v1/Locations?locality=Greenville&maxResults=10&key=${apiKey}`, {
+        method: 'GET'
+      })
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        this.landmark.name = json.name;
+      })
+      .catch((err) => console.error(err));
+    }
+  }
 }
+
+//const urlMap = 'http://dev.virtualearth.net/REST/v1/Locations/US/OH/43201/Columbus&maxResults={maxResults}&key=AmvR-c42ne6GrECkyJERi7B9mjs7vH-7OGFoG7jf405tiyb7huCJIfK1t_kn8S7m';
+const apiKey = 'AmvR-c42ne6GrECkyJERi7B9mjs7vH-7OGFoG7jf405tiyb7huCJIfK1t_kn8S7m';
+
 </script>
