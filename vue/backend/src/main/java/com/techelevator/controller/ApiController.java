3a1f9ca0fc2b53559a37b7f2fa1283533c9c16e5
Joseph Ramirez <joeyramirez.81@gmail.com>
2020-04-16 10:32:58 -0400

    added delete on api

--- vue/backend/src/main/java/com/techelevator/controller/ApiController.java
+++ vue/backend/src/main/java/com/techelevator/controller/ApiController.java
@@ -16,6 +16,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -102,4 +103,8 @@ public class ApiController {
 		itineraryDao.saveItineraryToDB(username, itineraryName, location);
 	}
 	
+	@DeleteMapping(path="/delete/{username}/{itineraryId}", consumes="application/json")
+	public void deleteItineraryFromDB(@PathVariable int itineraryId, @PathVariable String username) {
+	itineraryDao.deleteItinerary(username, itineraryId);
+}
 }
